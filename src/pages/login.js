b8ea7ef81d5812e7d10f47d9f0e12f856510de59
hyewon <w2106@e-mirim.hs.kr>
2023-08-22 14:25:18 +0900

    signup / login css

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from '../styles/sign.module.css';
+
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+};
 
 function App() {
     return (
-        <>
-        <form>
-            <input type="text" name="id" placeholder="이름"/>
-            <input type="password" name="password" placeholder="비밀번호"/>
-            <button type="submit">submit</button>
+        <div className={styles.container}>
+        <h1 className={styles.h1}>LOGIN</h1>
+        <form className={styles.form}>
+          <input
+            type="text"
+            name="id"
+            placeholder="ID"
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.submit}>SUBMIT</button>
         </form>
-        </>
+        <div className={styles["login-or-signup"]}>
+          <p className={styles["login-p"]}>계정이 없으신가요?</p>
+          <a className={styles["signup-to-login"]} href="/signup">회원가입</a>
+        </div>
+      </div>
     );
 }
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
+import styles from '../styles/sign.module.css';
 
 function App() {
-  const [formData, setFormData] = useState({ id: "", password: "" });
+  const [formData, setFormData] = useState({ name: "", id: "", password: "" });
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     try {
-    const response = await fetch("http://localhost:3000/save-info", {
+      const response = await fetch("http://localhost:3000/save-info", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-        });
-        
+      });
 
       if (response.ok) {
         console.log("Data saved successfully");
@@ -32,14 +32,24 @@ function App() {
   };
 
   return (
-    <>
-      <form onSubmit={handleFormSubmit}>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>SIGN UP</h1>
+      <form onSubmit={handleFormSubmit} className={styles.form}>
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={formData.name}
+          onChange={handleInputChange}
+          className={styles.input}
+        />
         <input
           type="text"
           name="id"
-          placeholder="이름"
+          placeholder="ID"
           value={formData.id}
           onChange={handleInputChange}
+          className={styles.input}
         />
         <input
           type="password"
@@ -47,10 +57,15 @@ function App() {
           placeholder="비밀번호"
           value={formData.password}
           onChange={handleInputChange}
+          className={styles.input}
         />
-        <button type="submit">submit</button>
+        <button type="submit" className={styles.submit}>SUBMIT</button>
       </form>
-    </>
+      <div className={styles["login-or-signup"]}>
+        <p className={styles["login-p"]}>계정이 있으신가요?</p>
+        <a className={styles["signup-to-login"]} href="/login">로그인</a>
+      </div>
+    </div>
   );
 }
 
